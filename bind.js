be40b4b2872b0ea5f2378c1b98c9bb50bf1a8176
yaoyan <yaoyan@souche.com>
2020-10-10 08:04:15 +0800

    new 写好了

--- bind.js
+++ bind.js
@@ -6,3 +6,30 @@
  * @Description: In User Settings Edit
  * @FilePath: /writeFunction/bind.js
  */
+Function.prototype.myBind = function(thisArg){
+    thisArg = thisArg == undefined ? window : thisArg;
+    const type = typeof thisArg;
+    const fn = Symbol('fn');
+    if(!['function','object'].includes(type)){
+        if(['bigint','symbol'].includes(type)){
+            thisArg = Object(thisArg);
+        }else {
+            thisArg = new thisArg.constructor(thisArg);
+        }
+    }
+    thisArg[fn] = this;
+    return  (...res)=> {
+        const result = thisArg[fn](...res);
+        delete thisArg[fn];
+        return result
+    }
+}
+
+var obj = {
+    name:1111
+}
+
+function  test() {
+    console.log(this.name);
+}
+test.myBind(obj)();
--- new.js
+++ new.js
@@ -6,3 +6,29 @@
  * @Description: In User Settings Edit
  * @FilePath: /writeFunction/new.js
  */
+
+function _new(fn,...res) {
+    const obj = Object.create(fn.prototype);
+    const result = fn.call(obj,...res);
+    if(result !== null && ['function','object'].includes(result)){
+        return result;
+    }
+    return obj; 
+}
+
+
+function Dog(name) {
+    this.name = name;
+}
+Dog.prototype.bark = function () {
+    console.log('wangwang');
+}
+Dog.prototype.sayName = function () {
+    console.log('my name is ' + this.name);
+}
+
+
+let sanmao = _new(Dog, '三毛');
+sanmao.bark(); //=>"wangwang"
+sanmao.sayName(); //=>"my name is 三毛"
+console.log(sanmao instanceof Dog); //=>true 
