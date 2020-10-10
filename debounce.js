51e36226b27e1c31ca68cf11ea4430d7fc9ef0ee
yaoyan <yaoyan@souche.com>
2020-10-10 18:48:02 +0800

    合并提交

--- debounce.js
+++ debounce.js
@@ -1,9 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2020-10-09 08:54:29
- * @LastEditTime: 2020-10-09 08:54:35
+ * @LastEditTime: 2020-10-10 11:45:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /writeFunction/debounce-throttle.js
  */
 
+// 防抖
+function debounce(fn,time=1000){
+    let _time = null;
+    return function(...arg){
+        clearTimeout(_time);
+        _time = setTimeout(()=>{
+            fn.call(this,...arg)
+        },time)
+    }
+}
